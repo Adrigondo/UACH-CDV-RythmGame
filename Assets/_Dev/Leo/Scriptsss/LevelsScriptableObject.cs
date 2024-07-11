845e097650ccf823bda5ea21fd50bac77eef5ba3
FranBulk <leo.bulkley@gmail.com>
2024-07-10 21:11:54 -0600

    Agregando la versión de los niveles 1.0

---
+++ Assets/_Dev/Leo/Scriptsss/LevelsScriptableObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEditor.SearchService;
+using UnityEngine;
+[CreateAssetMenu(fileName = "LevelScriptableobject", menuName = "ScriptableObjects/Level")]
+
+public class LevelsScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    string levelName;
+    public string LevelName {get => levelName; private set => levelName = value;}
+    
+    [SerializeField]
+    Image levelImage;
+    public Image LevelImage {get => levelImage; private set => levelImage = value;}
+
+    [SerializeField]
+    float maximumscore;
+    public float MaximumScore {get => maximumscore; private set => maximumscore = value;}
+
+    [SerializeField]
+    string levelScene;
+    public string LevelScene {get => levelScene; private set => levelScene = value;}
+}
