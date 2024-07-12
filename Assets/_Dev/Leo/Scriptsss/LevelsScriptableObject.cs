27336cedbfd988119958405396a64246ecea07da
Adora <im.stariluz@gmail.com>
2024-07-11 20:45:16 -0600

    Fix: image import

--- Assets/_Dev/Leo/Scriptsss/LevelsScriptableObject.cs
+++ Assets/_Dev/Leo/Scriptsss/LevelsScriptableObject.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.UI;
 [CreateAssetMenu(fileName = "LevelScriptableobject", menuName = "ScriptableObjects/Level")]
 
 public class LevelsScriptableObject : ScriptableObject
