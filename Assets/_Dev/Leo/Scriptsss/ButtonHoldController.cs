af41ec68eeffb4236e92d6402167adac3b4ad5b0
FranBulk <leo.bulkley@gmail.com>
2024-07-16 19:00:39 -0600

    Agregando barra de progresión al botón

--- Assets/_Dev/Leo/Scriptsss/ButtonHoldController.cs
+++ Assets/_Dev/Leo/Scriptsss/ButtonHoldController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public GameObject SceneChanger;
+    public Image progressButtonImage;
     public float holdTime; // Tiempo necesario para mantener el botón presionado
     private float holdTimer = 0f;
     private bool isHolding = false;
@@ -13,12 +14,15 @@ public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     void Start()
     {
         SceneChanger.SetActive(false);
+        progressButtonImage.GetComponent<Image>();
+        progressButtonImage.fillAmount = 0;
     }
     void Update()
     {
         if (isHolding)
         {
             holdTimer += Time.deltaTime;
+            progressButtonImage.fillAmount = holdTimer/holdTime;
             if (holdTimer >= holdTime)
             {
                 OnHoldComplete();
@@ -37,6 +41,7 @@ public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         isHolding = false;
         holdTimer = 0f;
+        progressButtonImage.fillAmount = 0;
     }
 
     private void OnHoldComplete()
