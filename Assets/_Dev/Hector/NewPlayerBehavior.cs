910aba539c5a7965531611cf75843d2c4bef813b
HeccielMedrano <146401143+HeccielMedrano@users.noreply.github.com>
2024-07-17 19:08:53 -0600

    Add: Teleportation and gravity flip mechanics
    Teleportation and gravity flip mechanics were added. Every TODO from before was fixed as well (and of course some more were added).

--- Assets/_Dev/Hector/NewPlayerBehavior.cs
+++ Assets/_Dev/Hector/NewPlayerBehavior.cs
@@ -1,7 +1,5 @@
-// TODO: MAKE THIS ACTUALLY WORK FOR WHEN THE PLAYER IS UPSIDE-DOWN OR RIGHTSIDE-UP.
-// TODO: MAKE THIS WORK FOR ALL ANGLES.
-// TODO: I SHOULD PROBABLY MAKE THE TELEPORT ITSELF INTO A DIFFERENT FUNCTION OR SOMETHING IDK.
-// TODO: NONE OF THIS WORKS IF THERE IS A COLLIDER THAT ISN'T WALKABLE IN THE WAY OF THE ACTUALLY WALKABLE GROUND.
+// TODO: Lock the teleport/gravity flip mechanics so they are only usable when grounded.
+// TODO: WHY DOES THE RAYCAST NOT DETECT THE GROUND WHAT IS GOING ON I'M SCARED.
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,15 +7,16 @@ using UnityEngine;
 
 public class NewPlayerBehavior : MonoBehaviour
 {
+    [SerializeField] protected float gravityScale = 9.81f;
     [SerializeField] protected float movementAngleInDegrees = 0;
     [SerializeField] protected float movementSpeed = 5f;
     protected float movementAngleInRadians = 0;
+    protected float playerHeight;
+    [SerializeField] protected float raycastLength = 15f;
     protected Collider2D playerCollider;
     protected Rigidbody2D rigidBody2D;
-    protected float playerHeight;
-    [SerializeField] protected float raycastLength = 10f;
 
-    void Start()
+    protected void Start()
     {
         playerCollider = GetComponent<Collider2D>();
         rigidBody2D = GetComponent<Rigidbody2D>();
@@ -29,17 +28,45 @@ public class NewPlayerBehavior : MonoBehaviour
         {
             Debug.LogError("No Collider2D found on the player");
         }
+        CheckIfGrounded();
     }
 
-    void Update()
+    protected void Update()
     {
-        movementAngleInRadians = movementAngleInDegrees * Mathf.Deg2Rad;
+        /* movementAngleInRadians = movementAngleInDegrees * Mathf.Deg2Rad;
         Vector2 movement = new Vector2(Mathf.Cos(movementAngleInRadians), Mathf.Sin(movementAngleInRadians));
-        transform.Translate(movement * movementSpeed * Time.deltaTime);
+        transform.Translate(movement * movementSpeed * Time.deltaTime); */
+        CheckIfGrounded();
 
         if (Input.GetMouseButtonDown(0))
         {
             CheckForWalkableTerrainAbove();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ChangePlayerGravityScale();
+        }
+    }
+
+    protected void CheckIfGrounded()
+    {
+        Vector2 raycastOrigin = transform.position;
+        raycastOrigin.y = playerHeight;
+        float raycastLength = 0.5f;
+        Vector2 downDirection = transform.TransformDirection(Vector2.down);
+        
+        RaycastHit2D[] hits = Physics2D.RaycastAll(raycastOrigin, downDirection, raycastLength);
+
+        foreach (RaycastHit2D hit in hits)
+        {   
+            if (hit.collider != null && hit.collider.CompareTag("Walkable"))
+            {
+                Debug.Log("PLAY IS GROUNDED I REPEAT PLAYER IS GROUNDED!");
+            }
+            else
+            {
+                Debug.Log("Player is not grounded!");
+            }
         }
     }
 
@@ -48,29 +75,42 @@ public class NewPlayerBehavior : MonoBehaviour
     // We know if the terrain is walkable if: a) it's a collider; and b) has tag 'Walkable'.
     // If it, indeed, is a terrain, the player is teleported exactly where the raycast hit ± half of the player's height...
     // this is made in order to make sure the player is not teleported inside of the collider.
+    // The aforementioned raycast will always point above the player, no matter the angle. As well as checking for every collider hit...
+    // until it finds a 'Walkable'. This means it will always stop at the first 'Walkable' found, even if there are more beyond the first one.
     /// </summary>
-    void CheckForWalkableTerrainAbove()
+    protected void CheckForWalkableTerrainAbove()
     {
         Vector2 raycastOrigin = transform.position;
-        raycastOrigin.y += (playerHeight / 2) + (playerHeight / 1000); //minus instead of plus when reversed
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast(raycastOrigin, Vector2.up, raycastLength);
+        raycastOrigin.y += (playerHeight / 2) + (playerHeight / 1000);
+        Vector2 upDirection = transform.TransformDirection(Vector2.up);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(raycastOrigin, upDirection, raycastLength);
 
-        if (hit.collider != null && hit.collider.CompareTag("Walkable"))
-        {
-            Vector2 impactPoint = hit.point;
-            Vector2 newPosition = new Vector2(impactPoint.x, impactPoint.y - playerHeight / 2); //plus instead of minus when reversed
-            transform.position = newPosition;
-            ChangePlayerGravityScale();
+        foreach (RaycastHit2D hit in hits)
+        {   
+            if (hit.collider != null && hit.collider.CompareTag("Walkable"))
+            {
+                Teleport(hit);
+                break;
+            }
         }
     }
 
-    void ChangePlayerGravityScale()
+    protected void Teleport(RaycastHit2D hit)
     {
-        rigidBody2D.gravityScale *= -1;
+        Vector2 impactPoint = hit.point;
+        Vector2 newPosition = new Vector2(impactPoint.x, impactPoint.y - playerHeight / 2);
+        transform.position = newPosition;
+        rigidBody2D.velocity = Vector2.zero;
+        ChangePlayerGravityScale();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected void ChangePlayerGravityScale()
+    {
+        rigidBody2D.gravityScale *= -1;
+        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z - 180);
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "GravityTrigger")
         {
@@ -85,7 +125,7 @@ public class NewPlayerBehavior : MonoBehaviour
         }
     }
 
-    private void SetGravityDirection(float angle)
+    protected void SetGravityDirection(float angle)
     {
         movementAngleInDegrees = angle;
         float radians = (angle - 90f) * Mathf.Deg2Rad;
@@ -98,10 +138,18 @@ public class NewPlayerBehavior : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
-    void OnDrawGizmos()
+    protected void OnDrawGizmos()
     {
-        Vector2 raycastOrigin = transform.position + Vector3.up * (playerHeight / 2 + (playerHeight / 1000));
+        Vector2 raycastOrigin = transform.position;
+        raycastOrigin.y += (playerHeight / 2) + (playerHeight / 1000);
+        Vector2 upDirection = transform.TransformDirection(Vector2.up);
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(raycastOrigin, raycastOrigin + Vector2.up * raycastLength);
+        Gizmos.DrawLine(raycastOrigin, raycastOrigin + upDirection * raycastLength);
+
+        Vector2 raycastOrigin2 = transform.position;
+        float raycastLength2 = playerHeight;
+        Vector2 downDirection = transform.TransformDirection(Vector2.down);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(raycastOrigin2, raycastOrigin2 + downDirection * raycastLength2);
     }
 }
