52d5f67e4ca4ac8a1acbe543b70ad21f8999f36c
Adora <im.stariluz@gmail.com>
2024-07-24 06:24:34 -0600

    Float action limited by time from first float in fall
    Now the float action will be stop after the limit time has been complete, and it will be able to use again once the player reach the ground.

--- Assets/_Dev/Stariluz/PlayerFloat.cs
+++ Assets/_Dev/Stariluz/PlayerFloat.cs
@@ -1,18 +1,23 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Stariluz
 {
-    public class PlayerFloat:MonoBehaviour
+    public class PlayerFloat : MonoBehaviour
     {
         #region "Fields"
         [SerializeField]
         [Tooltip("Time in seconds during wich the player is able to float. It starts since the first float in the current fall is activated.")]
-        protected float TimeLimit;
+        protected float TimeLimit = 5;
 
         /// <summary>
         /// Main class of every character
         protected NewPlayerBehavior Behaviour;
+        protected bool CanFloat = true;
+
+        protected Coroutine TimerRoutine;
+
         #endregion
 
         #region "LifeCycle methods"
@@ -20,7 +25,19 @@ namespace Stariluz
         {
             Behaviour = GetComponent<NewPlayerBehavior>();
         }
+        protected void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Walkable")
+            {
+                CanFloat = true;
+                if (TimerRoutine != null){
+                    StopCoroutine(TimerRoutine);
+                    TimerRoutine=null;
+                }
+            }
+        }
         #endregion
+
         #region "Public methods"
         public void OnFloat(InputAction.CallbackContext context)
         {
@@ -33,10 +50,6 @@ namespace Stariluz
             }
             if (context.performed)
             {
-                // if(context.interaction is HoldInteraction){
-
-                //     Float();
-                // }
                 StopFloat();
             }
         }
@@ -44,14 +57,22 @@ namespace Stariluz
 
         protected void Float()
         {
-            Behaviour.RigidBody2D.gravityScale = 0;
-            Behaviour.RigidBody2D.velocity = Vector2.zero;
+            if(CanFloat){
+                Behaviour.RigidBody2D.gravityScale = 0;
+                Behaviour.RigidBody2D.velocity = Vector2.zero;
+                if(TimerRoutine==null) TimerRoutine = StartCoroutine(RunTimer());
+            }
         }
 
         protected void StopFloat()
         {
             Behaviour.ResetGravityScale();
         }
-
+        IEnumerator RunTimer()
+        {
+            yield return new WaitForSeconds(TimeLimit);
+            StopFloat();
+            CanFloat=false;
+        }
     }
 }
